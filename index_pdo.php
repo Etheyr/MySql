94fe2ab9a95779a6dcb52a9dda3a0f5bf204c62b
axel <axel.bouillart@gmail.com>
2017-01-31 16:51:19 +0100

    affichage avec les requête prepapés

--- index_pdo.php
+++ index_pdo.php
@@ -1,6 +1,8 @@
 <?php
 $bdd = new PDO('mysql:host=localhost;dbname=mon_armoire;charset=utf8','root','SIMPLONCO');
 $response = $bdd->query("SELECT * FROM mes_chaussettes;");
+$req = $bdd->prepare('SELECT couleur , pointure FROM mes_chaussettes WHERE couleur = ? AND pointure = ?');
+$req->execute(array($_GET['couleur'], $_GET['pointure']));
 ?>
 
 <!DOCTYPE html>
@@ -11,19 +13,17 @@ $response = $bdd->query("SELECT * FROM mes_chaussettes;");
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
 </head>
 <body>
-<h1>Mes données :</h1>
+	<h1>Mes données :</h1>
 
 	<table class="ui table">
 
-		<?php while($donnees = $response->fetch()):?>
+		<?php while($donnees = $req->fetch()):?>
 
 			<tr>
-			<td></td>
-				<td><?php echo $donnees['id'];?></td>
+
 				<td><?php echo $donnees['couleur'];?></td>
 				<td><?php echo $donnees['pointure'];?></td>
-				<td><?php echo $donnees['temp_lavage'];?></td>
-				<td><?php echo $donnees['date_lavage'];?></td>
+
 			</tr>
 
 		<?php endwhile;?>
